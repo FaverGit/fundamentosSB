ca32aee4bbb729817b4e9b0ea802755b52cfb86f
Faver <fjisaza@personalsoft.com.co>
2022-04-21 10:42:37 -0500

    first commit

---
+++ src/test/java/com/fundamentosplatzi/springboot/fundamentosSB/FundamentosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentosSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
